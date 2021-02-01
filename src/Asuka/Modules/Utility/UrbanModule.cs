2feff3383a4ccdba6b1196b425101b5c8f2c9bda
Brian Lai <brianlai30@outlook.com>
2021-01-31 23:54:24 -0800

    Added embed to Urban command, not implemented.

--- src/Asuka/Modules/Utility/UrbanModule.cs
+++ src/Asuka/Modules/Utility/UrbanModule.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Asuka.Commands;
 using Asuka.Configuration;
+using Discord;
 using Discord.Commands;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Options;
@@ -21,7 +22,28 @@ namespace Asuka.Modules.Utility
         {
             var url = "https://api.urbandictionary.com/v0/define";
             var query = QueryHelpers.AddQueryString(url, "term", term);
-            await ReplyAsync(query);
+
+            var embed = new EmbedBuilder()
+                .WithTitle("entry.word")
+                .WithUrl("https://www.google.com/")
+                .WithColor(0xefff00)
+                .WithAuthor("Urban Dictionary", Context.Guild.IconUrl)
+                .WithDescription("Written on: parse date entry.written_on")
+                .WithFooter(
+                    "👍 entry.thumbsUp\n" +
+                    "👎 entry.thumbsDown")
+                .AddField(
+                    "Definition",
+                    "entry.definition trim to 1024 char")
+                .AddField(
+                    "Example",
+                    "entry.example trim to 1024 char")
+                .AddField(
+                    "Author",
+                    "entry.author")
+                .Build();
+
+            await ReplyAsync(query, embed: embed);
         }
     }
 }
