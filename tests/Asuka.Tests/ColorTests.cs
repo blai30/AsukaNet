464eefa371a540f5ae567a88a5f624675f69ec45
Brian Lai <brianlai30@outlook.com>
2021-02-05 18:41:30 -0800

    tests: add Asuka.Test NUnit project

---
+++ tests/Asuka.Tests/ColorTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Asuka.Tests
+{
+    public class ColorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
