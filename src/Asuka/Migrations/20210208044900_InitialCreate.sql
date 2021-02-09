e9027273e8dd2a7a468f8b523085fa1bbbbdc616
Brian Lai <brianlai30@outlook.com>
2021-02-08 21:53:51 -0800

    fix: update InitialCreate sql script to auto increment id
    Also changed the size of name and content

--- src/Asuka/Migrations/20210208044900_InitialCreate.sql
+++ src/Asuka/Migrations/20210208044900_InitialCreate.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE tags
 (
-    id          INT           NOT NULL PRIMARY KEY,
-    name        NVARCHAR(200) NOT NULL,
-    content     NVARCHAR(500) NOT NULL,
+    id          INT           NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name        NVARCHAR(100) NOT NULL,
+    content     NVARCHAR(255) NOT NULL,
     user_id     BIGINT        NOT NULL,
     guild_id    BIGINT        NOT NULL,
     usage_count INT           NOT NULL DEFAULT 0
