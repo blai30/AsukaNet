40dd53e1d90a9c956e007a32f3f09d2e709674b2
Brian Lai <brianlai30@outlook.com>
2021-02-08 21:49:46 -0800

    refactor: reformat InitialCreate sql script for alignment

--- src/Asuka/Migrations/20210208044900_InitialCreate.sql
+++ src/Asuka/Migrations/20210208044900_InitialCreate.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE tags
 (
-    id INT NOT NULL PRIMARY KEY,
-    name NVARCHAR(200) NOT NULL,
-    content NVARCHAR(500) NOT NULL,
-    user_id BIGINT NOT NULL,
-    guild_id BIGINT NOT NULL,
-    usage_count INT NOT NULL DEFAULT 0
+    id          INT           NOT NULL PRIMARY KEY,
+    name        NVARCHAR(200) NOT NULL,
+    content     NVARCHAR(500) NOT NULL,
+    user_id     BIGINT        NOT NULL,
+    guild_id    BIGINT        NOT NULL,
+    usage_count INT           NOT NULL DEFAULT 0
 );
