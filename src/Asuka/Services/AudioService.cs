6ec3cbfd8909d0f93069295ee537239a307a457a
Brian Lai <brianlai30@outlook.com>
2021-02-20 21:02:45 -0800

    feat: AudioService and MusicModule now plays for the most part
    Careful when kicking the bot from a voice channel as it won't be able to join back until it stops playing music.

--- src/Asuka/Services/AudioService.cs
+++ src/Asuka/Services/AudioService.cs
@@ -4,6 +4,7 @@ using Discord.WebSocket;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Victoria;
+using Victoria.EventArgs;
 
 namespace Asuka.Services
 {
@@ -26,6 +27,8 @@ namespace Asuka.Services
         public async Task StartAsync(CancellationToken cancellationToken)
         {
             _client.Ready += OnReadyAsync;
+            _lavaNode.OnTrackStarted += OnTrackStarted;
+            _lavaNode.OnTrackEnded += OnTrackEnded;
 
             _logger.LogInformation($"{GetType().Name} started");
             await Task.CompletedTask;
@@ -34,6 +37,8 @@ namespace Asuka.Services
         public async Task StopAsync(CancellationToken cancellationToken)
         {
             _client.Ready -= OnReadyAsync;
+            _lavaNode.OnTrackStarted -= OnTrackStarted;
+            _lavaNode.OnTrackEnded -= OnTrackEnded;
 
             _logger.LogInformation($"{GetType().Name} stopped");
             await Task.CompletedTask;
@@ -46,6 +51,33 @@ namespace Asuka.Services
                 await _lavaNode.ConnectAsync();
                 _logger.LogInformation("Lava Node connected");
             }
+        }
+
+        private async Task OnTrackStarted(TrackStartEventArgs args)
+        {
+            var player = args.Player;
+            _logger.LogTrace($"Playing: {args.Track.Title} in {args.Player.VoiceChannel.Guild.Name}");
+            await player.TextChannel.SendMessageAsync($"Playing: `{player.Track.Title}`.");
+        }
+
+        private async Task OnTrackEnded(TrackEndedEventArgs args)
+        {
+            if (!args.Reason.ShouldPlayNext()) {
+                return;
+            }
+
+            var player = args.Player;
+            await player.TextChannel.SendMessageAsync($"{args.Reason}: `{args.Track.Title}`.");
+            await player.TextChannel.SendMessageAsync($"`{player.Queue.Count}` tracks left in the queue.");
+
+            // Check next track in the queue or leave voice channel if queue is empty.
+            if (!player.Queue.TryDequeue(out var track)) {
+                await _lavaNode.LeaveAsync(player.VoiceChannel);
+                return;
+            }
+
+            // Play next track in the queue.
+            await player.PlayAsync(track);
         }
     }
 }
